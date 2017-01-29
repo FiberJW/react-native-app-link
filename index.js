37e17243841ee5f4c7de8a85cf8b6ace534a1b2c
Juwan Wheatley <datwheat@gmail.com>
2017-01-29 18:37:15 -0500

    fixed a typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export const maybeOpenURL = async (url, { appName, appStoreId, playStoreId }) =>
       if (Platform.OS === 'ios') {
         Linking.openURL(`https://itunes.apple.com/us/app/${appStoreId}`)
       } else {
-        Linking.openURL(`https://play.google.com/store/apps/details?id={playStoreId}`)
+        Linking.openURL(`https://play.google.com/store/apps/details?id=${playStoreId}`)
       }
     } else {
       throw new Error(`Could not open ${appName}. ${err.toString()}`)
@@ -21,7 +21,7 @@ export const openInStore = async (appStoreId, playStoreId) => {
   if (Platform.OS === 'ios') {
     Linking.openURL(`https://itunes.apple.com/us/app/${appStoreId}`)
   } else {
-    Linking.openURL(`https://play.google.com/store/apps/details?id={playStoreId}`)
+    Linking.openURL(`https://play.google.com/store/apps/details?id=${playStoreId}`)
   }
 }
 
