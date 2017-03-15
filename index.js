55d7e35a15b89e69b094a4888b04a2a321dee36e
Juwan Wheatley <datwheat@gmail.com>
2017-03-15 10:41:34 -0400

    move to prettier

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-import {
-  Linking,
-  Platform
-} from 'react-native'
+import { Linking, Platform } from 'react-native';
 
-export const maybeOpenURL = async (url, { appName, appStoreId, playStoreId }) => {
+export const maybeOpenURL = async (
+  url,
+  { appName, appStoreId, playStoreId }
+) => {
   Linking.openURL(url).catch(err => {
     if (err.code === 'EUNSPECIFIED') {
       if (Platform.OS === 'ios') {
-        Linking.openURL(`https://itunes.apple.com/us/app/${appStoreId}`)
+        Linking.openURL(`https://itunes.apple.com/us/app/${appStoreId}`);
       } else {
-        Linking.openURL(`https://play.google.com/store/apps/details?id=${playStoreId}`)
+        Linking.openURL(
+          `https://play.google.com/store/apps/details?id=${playStoreId}`
+        );
       }
     } else {
-      throw new Error(`Could not open ${appName}. ${err.toString()}`)
+      throw new Error(`Could not open ${appName}. ${err.toString()}`);
     }
-  })
-}
+  });
+};
 
 export const openInStore = async (appStoreId, playStoreId) => {
   if (Platform.OS === 'ios') {
-    Linking.openURL(`https://itunes.apple.com/us/app/${appStoreId}`)
+    Linking.openURL(`https://itunes.apple.com/us/app/${appStoreId}`);
   } else {
-    Linking.openURL(`https://play.google.com/store/apps/details?id=${playStoreId}`)
+    Linking.openURL(
+      `https://play.google.com/store/apps/details?id=${playStoreId}`
+    );
   }
-}
+};
 
 export default {
   maybeOpenURL,
-  openInStore
-}
+  openInStore,
+};
