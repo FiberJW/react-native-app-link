593b1ab65ad962837ac0dc6e0054d7515f1a65ed
Juwan Wheatley <datwheat@gmail.com>
2021-03-29 11:55:47 -0400

    fix typo

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ export const maybeOpenURL = async (
 
 export const openInStore = async ({ appName, appStoreId, appStoreLocale = 'us', playStoreId }) => {
   if (Platform.OS === 'ios') {
-    Linking.openURL(`https://apps.apple.com/${locale}/app/${appName}/id${appStoreId}`);
+    Linking.openURL(`https://apps.apple.com/${appStoreLocale}/app/${appName}/id${appStoreId}`);
   } else {
     Linking.openURL(
       `https://play.google.com/store/apps/details?id=${playStoreId}`
